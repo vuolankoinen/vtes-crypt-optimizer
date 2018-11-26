8a1b4f372b95af44b87c975fc81cf2a9b21f2de1
Matti Palomäki <vuolankoinen@users.noreply.github.com>
2018-11-26 17:58:15 +0200

    Arvioiden viilailua

--- kryptan-hyvyys.R
+++ kryptan-hyvyys.R
@@ -1,6 +1,6 @@
 ## Aloituskryptien arvioinnin kriteeri
 
-kryptan_hyvyys <- function(krypta, n=10) {
+kryptan_hyvyys <- function(krypta, n=100) {
   ## Puuttuu: tittelit, tarkemmat capacity-arvot
   tulos <- 0
   for (kk in 1:n) { ## Monia simuloituja nostoja
@@ -8,16 +8,15 @@ kryptan_hyvyys <- function(krypta, n=10) {
     kr <- krypta[koe,]
     ekat_4 <- unique(kr[1:4,]) # Tuplat eivät auta
     { # Varsinainen arviointi
-      tulos <- tulos + nrow(ekat_4) # Jokainen pyyri on jonkin arvoinen
       tulos <- tulos + sum(10-ekat_4[, "cap"]) # Pienemmät on parempia
       tulos <- tulos + sum(ekat_4[, "dom"]) # Enemmän domia on parempi
-      tulos <- tulos + length(which(ekat_4[, "dom"]!=0)) # Ylipäätään dom
-      tulos <- tulos + sum(ekat_4[, "tha"])*0.75  # Enemmän thaa on parempi
-      tulos <- tulos + length(which(ekat_4[, "tha"]!=0))*0.75 # Ylipäätään tha
+      tulos <- tulos + 1.5*length(which(ekat_4[, "dom"]!=0)) # Ylipäätään dom
+      tulos <- tulos + sum(ekat_4[, "tha"])*1  # Enemmän thaa on parempi
+      tulos <- tulos + length(which(ekat_4[, "tha"]!=0))*1 # Ylipäätään tha
       tulos <- tulos - length(which(ekat_4[, "title"]!=0)) # Tittelit miinusta, ei voi mennä anarkistiksi
-      if (sum(ekat_4[,"BH"])==0) { # Ei Black Handiä: haitta kuin kolme pyyriä ilman domia
-        tulos  <- tulos-9
-        if (kr[5,"BH"]==0) tulos <- tulos - 2 # ehtii kalastaa, lisämiinus jos ei sieltäkään löydy
+      if (sum(ekat_4[,"BH"])==0) { # Ei Black Handiä: haitta kuin kolme pyyriä ilman domia...
+        tulos  <- tulos-7
+        if (kr[5,"BH"]==0) tulos <- tulos - 2 # ...jos ei kalastamallakaan löydy
       }
       if (sum(ekat_4[,"aus"])==0) {tulos <- tulos -9}# Ei ausia: haitta kuin kolme pyyriä ilman domia
       if (sum(ekat_4[,"nec"])==0) {tulos <- tulos -8}# Ei neciä: haitta hiukan pienempi kuin ausin puutteessa
@@ -25,7 +24,7 @@ kryptan_hyvyys <- function(krypta, n=10) {
       tulos <- tulos + sum(ekat_4[, "special"]) # Itse käsin merkityt arvot spessuille
       tulos <- tulos + sum(ekat_4[, "pre"])*0.1 + sum(kr[1:4, "pro"])*0.1 # Sivuhyötyjä anarkkikorteista
       sup_gov <- 0
-      for (tt in unique(sort(ekat_4[,"cap"], decreasing=TRUE))) { # DOM-pyyriä pienemmät pyyrit ovat hyviä (sup-Govern-ketju)
+      for (tt in sort(ekat_4[,"cap"], decreasing=TRUE)) { # DOM-pyyriä pienemmät pyyrit ovat hyviä
         tulos <- tulos + sup_gov * length(which(ekat_4[,"cap"]==tt))
         if (length(which(ekat_4[which(ekat_4[,"dom"]==2),"cap"]==tt))>0) {
           sup_gov <- sup_gov + 1
